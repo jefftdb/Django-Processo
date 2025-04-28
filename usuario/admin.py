25d965a4414a68c8d5a6cdcf22f8d8a1cae0fc2e
jefftdb <jefftdb@gmail.com>
2025-04-27 21:18:53 -0300

    Alterei minha senha superuser

--- usuario/admin.py
+++ usuario/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from usuario.models import Usuario
 
 class UsuarioAdmin(admin.ModelAdmin):
-    list_display = ("id", "username", "first_name", "last_name", "telefone", "email")
+    list_display = ("id", "username", "first_name", "last_name","CPF", "telefone", "email","is_staff","is_active")
 
     search_fields = ("id", "username","email")
 
